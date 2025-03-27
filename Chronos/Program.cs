12bdafd3466cec5b080cba46a5f30937970dcb46
Syndher <miguelfilipe.dev@gmail.com>
2025-03-27 20:34:35 +0000

    Crono1 and Crono2 completed

---
+++ Chronos/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Diagnostics;
+
+namespace Chronos
+{
+
+    class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+            crono1.Start();
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(250);
+            crono1.Stop();
+            crono2.Stop();
+            Console.WriteLine("{0:F4}", crono1.Elapsed.TotalSeconds);
+            Console.WriteLine("{0:F4}", crono2.Elapsed.TotalSeconds);
+        }
+
+    }
+}
