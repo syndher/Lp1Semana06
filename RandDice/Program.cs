d830824c98ef5ce98767fd26710a1cf53a21c5c2
Syndher <miguelfilipe.dev@gmail.com>
2025-03-27 20:54:43 +0000

    Add sum of the rolls

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -8,9 +8,14 @@ namespace Chronos
 
         private static void Main(string[] args)
         {
+            int result = 0;
             Random r1 = new Random(Convert.ToInt32(args[1]));
             for (int i = 0; i < Convert.ToInt32(args[0]); i++)
-            Console.WriteLine(r1.Next(1,7));
+            {
+                int numeros = r1.Next(1,7);
+                result += numeros;
+            }
+            Console.WriteLine(result);
         }
 
     }
