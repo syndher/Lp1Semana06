bb2fb39eb9a96e06169cb4314070bcd27680c79c
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-03-28 16:48:53 +0000

    Add Enemy.cs and all c# code to MyRoguelike

---
+++ MyRoguelike/Enemy.cs
@@ -0,0 +1,61 @@
+﻿namespace MyGame
+{
+    public class Enemy
+    {
+
+        
+        private string name;
+        public string GetName() => name;
+        private float health;
+        public float GetHealth() => health;
+        private float shield;
+        public float GetShield() => shield;
+
+        private static int powerUpCount;
+        public static int GetPowerUpCount() => powerUpCount;
+        
+        static Enemy()
+        {
+            powerUpCount = 0;
+        }
+        
+        public void SetName(string newName)
+        {
+            if (newName.Length > 8) newName = newName.Substring(0, 8);
+            name = newName;
+        }
+        
+        public Enemy(string name)
+        {
+            SetName(name);
+            health = 100;
+            shield = 0;
+        }
+
+        public void PickPowerUp(Program.PowerUp powerUp, float value)
+        {
+            powerUpCount++;
+            if (powerUp == Program.PowerUp.SHIELD)
+            {
+                shield += value;
+                if (shield > 100) shield = 100;
+                return;
+            }
+            health += value;
+            if (health > 100) health = 100;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageLeft = -shield;
+                shield = 0;
+                health -= damageLeft;
+                if (health < 0) health = 0;
+            }
+        }
+        
+    }
+}
--- MyRoguelike/Program.cs
+++ MyRoguelike/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyRoguelike
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            List<Enemy> enemies = new List<Enemy>();
+            for (int i = 0; i < int.Parse(args[0]); i++)
+            {
+                Console.Write($"Nome do inimigo {i+1}: ");
+                string input = Console.ReadLine();
+                enemies.Add(new Enemy(input));
+            }
+            
+            foreach (Enemy enemy in enemies)
+            {
+                Console.WriteLine($"{enemy.GetName()} {enemy.GetHealth()} {enemy.GetShield()}");
+            }
+            
+            enemies[0].TakeDamage(50);
+            
+            foreach (Enemy enemy in enemies)
+            {
+                Console.WriteLine($"{enemy.GetName()} {enemy.GetHealth()} {enemy.GetShield()}");
+            }
+            
+            enemies[1].PickPowerUp(PowerUp.HEALTH, 50);
+            enemies[0].PickPowerUp(PowerUp.SHIELD, 9.5f);
+            
+            foreach (Enemy enemy in enemies)
+            {
+                Console.WriteLine($"{enemy.GetName()} {enemy.GetHealth()} {enemy.GetShield()}");
+            }
+            
+            
+            Console.WriteLine($"Número de powerups usados: {Enemy.GetPowerUpCount()}");
+            
+        }
+        
+        public enum PowerUp {
+            HEALTH, SHIELD
+        }
+
+        public enum Toughness
+        {
+            Very Easy, Easy, Normal, Tough, Nightmare
+        }
+    }
+}
